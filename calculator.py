c0bc72e2c590f942d9bf27907e98caa33797343a
Purushothaman <Karthik.Purushothaman@kla-tencor.com>
2020-07-04 11:01:45 +0530

    include recurring income in returns calculation

--- calculator.py
+++ calculator.py
@@ -2,7 +2,8 @@ from math import pow
 
 
 class Calculator:
-    def calculateLumpsumReturns(self, args:dict) -> float:
+    def calculateLumpsumReturns(self, args: dict) -> float:
+        presentValue = 0
         if 'presentValue' in args:
             presentValue = float(args['presentValue'])
 
@@ -10,10 +11,27 @@ class Calculator:
             rateOfReturn = float(args['rateOfReturn'])
 
         if 'investmentDurationInYears' in args:
-            investmentDurationInYears = float(args['investmentDurationInYears'])
+            investmentDurationInYears = float(
+                args['investmentDurationInYears'])
 
         if 'numberOfCompoundInterestInYear' in args:
-            numberOfCompoundInterestInYear = int(args['numberOfCompoundInterestInYear'])
+            numberOfCompoundInterestInYear = int(
+                args['numberOfCompoundInterestInYear'])
+
+        recurringMonthlyInvestment = 0
+        if 'recurringMonthlyInvestment' in args:
+            recurringMonthlyInvestment = int(
+                args['recurringMonthlyInvestment'])
 
         rateOfReturn = rateOfReturn/100
+        lumpSum = self.calculateLumpsum(presentValue, rateOfReturn, investmentDurationInYears, numberOfCompoundInterestInYear)
+        recurringSum = self.calculateRecurringInvestmentMaturity(presentValue, rateOfReturn, investmentDurationInYears, numberOfCompoundInterestInYear, recurringMonthlyInvestment)
+        return  lumpSum + recurringSum
+
+    def calculateLumpsum(self, presentValue: float, rateOfReturn: float, investmentDurationInYears: float, numberOfCompoundInterestInYear: float):
         return presentValue * (pow((1+(rateOfReturn/numberOfCompoundInterestInYear)), (numberOfCompoundInterestInYear * investmentDurationInYears)))
+
+    def calculateRecurringInvestmentMaturity(self,  presentValue: float, rateOfReturn: float, 
+    investmentDurationInYears: float, numberOfCompoundInterestInYear: float, recurringMonthlyInvestment: float):
+        numberOfQuaters = investmentDurationInYears*4.0
+        return recurringMonthlyInvestment * (pow((1+(rateOfReturn/4)),numberOfQuaters)-1)/(1-(pow((1+(rateOfReturn/4)),(-1/3))))
